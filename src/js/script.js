4410a623c11265695e264ee7815aa8a37d410936
Fernandounuts <fernando.balieiro0580@hotmail.com>
2023-05-26 22:15:54 -0300

    corrijido a parte do código em qual não era trocado os jogadores

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,46 @@
 "use strict";
 
 // Selecting elements
-const score0El = document.querySelector("#score--0");
+const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
-const diceEl = document.querySelector(".dice");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+let scores, currentScore, activePlayer, playing;
+
+scores = [0, 0];
+currentScore = 0;
+activePlayer = 0;
+
+// starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add('hidden')
+current1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+// Rolling dice function
+btnRoll.addEventListener("click", function () {
+  // 1. Generating random dice roll
+  const dice = Math.trunc(Math.random() * 6 + 1);
+  console.log(dice);
+  // 2. Display the dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `src/img/dice-${dice}.png`;
+  // 3. Check for rolled 1: if true, switch to another player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
